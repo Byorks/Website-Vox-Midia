1b932c8669ee180da44c8715031d4b644ac49562
Byorks <vanessabyorksp@gmail.com>
2025-05-13 00:16:27 -0300

    creating pages"

--- vox-midia-website/src/App.jsx
+++ vox-midia-website/src/App.jsx
@@ -1,7 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router'
+
 import Header from './components/Header'
 import MainFooter from './components/Footer'
 import { useState } from 'react'
 import './App.css'
+
+const router = createBrowserRouter ([
+  {
+    path:"/",
+    element: <RootLayout />,
+    children: []
+  }
+])
+
 
 function App() {
 
