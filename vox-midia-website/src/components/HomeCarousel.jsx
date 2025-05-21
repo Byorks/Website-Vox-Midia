4115057d68de504d1fa6f7b5becf2ca70eb63c79
Byorks <vanessabyorksp@gmail.com>
2025-05-20 22:41:29 -0300

    :construction: inprog: creating responsive banner

--- vox-midia-website/src/components/HomeCarousel.jsx
+++ vox-midia-website/src/components/HomeCarousel.jsx
@@ -1,4 +1,6 @@
-import  gnvImage  from '../assets/banners/gnv-img.jpg'
+import  introBanner  from '../assets/banners/banner-intro.png';
+import introBannerX2 from '../assets/banners/banner-intro-x2.png';
+import introBannerGnv from '../assets/banners/gnv-img.jpg';
 
 import {
   Carousel,
@@ -10,6 +12,8 @@ import {
 import Autoplay from 'embla-carousel-autoplay';
 
 export default function HomeCarousel() {
+  let srcSetList = introBanner + " 1440w " + introBannerX2 + " 2880w";
+  console.log(srcSetList)
   return (
     <Carousel 
         className="w-full"
@@ -21,9 +25,13 @@ export default function HomeCarousel() {
     >
       <CarouselContent className="w-full -ml-0 ">
         {Array.from({ length: 2 }).map((_, index) => (
-          <CarouselItem className='basis-full pl-0 max-h-[453px]' key={index}>
-            <div className="flex h-full w-full ">
-                <img className='object-cover' src={gnvImage} alt="" />
+          <CarouselItem className='basis-full pl-0 ' key={index}>
+            <div className="flex h-auto w-full ">
+                <img className='object-cover' src={introBanner}
+                    srcSet={srcSetList}
+                    sizes='(max-width: 1440px) 1440px, 2880px'
+                    alt="" 
+                />
             </div>
           </CarouselItem>
         ))}
