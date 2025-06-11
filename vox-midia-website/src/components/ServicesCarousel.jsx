2c7448835ee3ce98ef148fe05acbe360a0829401
VANESSA BYORK FERREIRA PINTO <52865485803@iMac-de-command23.local>
2025-06-11 08:56:29 -0300

    fix: carrossel responsiveness

--- vox-midia-website/src/components/ServicesCarousel.jsx
+++ vox-midia-website/src/components/ServicesCarousel.jsx
@@ -25,7 +25,7 @@ export default function ServicesCarousel({cardList}) {
                 {cardList.map((card, index) => (
                   // Aqui controla o tamanho de cada div
                   // TODO -> Fazer aparecer 2 cards no tablet
-                  <CarouselItem className="basis-1/1 g:basis-1/3 pl-3" key={index}> 
+                  <CarouselItem className="basis-1/1 lg:basis-1/3 pl-3" key={index}> 
                     <div className="flex justify-around">
                         <ServiceDescriptionCard {...card}></ServiceDescriptionCard>
                     </div>
